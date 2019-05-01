7227b41ba300a11aa076aa04756d8a11035f75d4
Anton De Meester <anton@0smosis.com>
2019-05-01 16:55:40 +0200

    / too much

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://library-backend-transvaal.herokuapp.com/',
+  backendUrl: 'https://library-backend-transvaal.herokuapp.com',
 };
